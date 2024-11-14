3f4ceed3ccb23c2732a8608a15a9480b0bb07dca
Abhay-kashyap <abhaykashyap265@gmail.com>
2024-11-14 17:36:19 +0530

    add assignmentoperator file

---
+++ JavaOperators/AssignmentOperators.java
@@ -0,0 +1,12 @@
+package JavaOperators;
+
+public class AssignmentOperators {
+    public static void main(String[] args) {
+        int a=10;
+        // a=a+10;
+        // a+=10;
+        int b=5;
+        b%=5;
+        System.out.println(b);
+    }
+}
