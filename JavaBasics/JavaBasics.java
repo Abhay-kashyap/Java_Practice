3c1ffa8ae309e6682dc4c0247cb9a5aaf3aede5d
Abhay-kashyap <abhaykashyap265@gmail.com>
2024-11-14 16:44:49 +0530

    add Java Basic File

---
+++ JavaBasics/JavaBasics.java
@@ -0,0 +1,120 @@
+package JavaBasics;
+import java.util.*;
+
+public class JavaBasics{
+    public static void main(String args[]){
+        // System.out.println("Hello world");
+      
+        // System.out.println("Hello world \n how are you \n i am fine and you");
+    //  int a =10,b=5;
+
+    //  System.out.println(a);
+    //  System.out.println(b);
+    //  String name="tony stark";
+    //  System.out.println(name);
+    //  a=b;
+    //  System.out.println(a);
+
+
+    // data type in java 
+    // byte b=8 ;
+    // System.out.println(b);
+    // char ch='a';
+    // System.out.println(ch);
+    // boolean var=true;
+    // System.out.println(var);
+    // float price =  10.5f;
+    // System.out.println(price);
+    // int number=25;
+    // long val=12445578555l;
+    // double longdec=1.22456544d;
+    // System.out.println(longdec);
+    // short n=250;
+    // System.out.println(n);
+    // sum of two numbers
+    // int num1=10;
+    // int num2 =2;
+    // int sum=num1+num2;
+    // System.out.println(sum);
+
+    /**Comments in Java */
+    // single line comments 
+    /*this is a multiline comments .
+    this helpful for code understanding
+    */
+
+
+    // taking input in java
+    // Scanner sc=new Scanner(System.in);
+    // take string only one sentence before spaces
+    // String input=  sc.next();
+    // System.out.println(input);
+    
+    // take whole string after spaces
+//    String fullName =sc.nextLine();
+//    System.out.println(fullName);
+
+//    taking number input 
+// int number=sc.nextInt();
+// System.out.println(number);
+
+// float prices=sc.nextFloat();
+// System.out.println(prices);
+// double large=sc.nextDouble();
+// System.out.println(large);
+// Short sh=sc.nextShort();
+// System.out.println(sh);
+// long largest=sc.nextLong();
+// System.out.println(largest);
+
+// sum of two number
+Scanner sc=new Scanner(System.in);
+// int a=sc.nextInt();
+// int b=sc.nextInt();
+// int sum=a+b;
+// System.out.println("The sum of two numbers is " + sum);
+
+// product of two numbers;
+// int a =sc.nextInt();
+// int b=sc.nextInt();
+// int product=a*b;
+// System.out.println(product);
+
+// area of circle
+// float rad=sc.nextFloat();
+// float area=3.14f * (rad*rad);
+// System.out.println(area);
+
+// type conversion
+// int a=25;
+// long b=a;
+
+// this is not possible
+// long a=25;
+// int b=a;
+
+// System.out.println(b);
+// float number=sc.nextInt();
+// System.out.println(number);
+// int n=10;
+// float fl=n;
+// float fl=20;
+// int n=fl;
+// System.out.println(n);
+
+// type casting
+// float a=25.12f;
+// int b= (int)a;
+// System.out.println(b);
+// float marks=99.999f;
+// int marks2=(int)marks;
+// System.out.println(marks2);
+char ch='a';
+int num=ch;
+System.out.println(num);
+
+
+
+    }
+}
+//boilerplate code
