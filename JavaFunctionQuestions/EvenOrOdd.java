64872a83187bd699b617c44dff462e52848b21d7
Abhay-kashyap <abhaykashyap265@gmail.com>
2024-11-27 23:53:06 +0530

    add isEven program

---
+++ JavaFunctionQuestions/EvenOrOdd.java
@@ -0,0 +1,15 @@
+package JavaFunctionQuestions;
+
+public class EvenOrOdd {
+    public static boolean isEven(int n){
+        if(n%2==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(isEven(5));
+    }
+}
