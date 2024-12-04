aa9d01db56b2cfe9d7395931fb8fbe183f5c8a96
Abhay-kashyap <abhaykashyap265@gmail.com>
2024-12-04 12:54:06 +0530

    add solidRhombus file

---
+++ JavaPatternPractice/solidRhombus.java
@@ -0,0 +1,20 @@
+package JavaPatternPractice;
+
+public class solidRhombus {
+    public static void solidRhombusPattern(int n){
+        for(int i=1;i<=n;i++){
+            // spaces
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            // stars
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        solidRhombusPattern(5);
+    }
+}
