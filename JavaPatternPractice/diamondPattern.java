3de024eb1c917e978aa6f335d39c20c59149f06a
Abhay-kashyap <abhaykashyap265@gmail.com>
2024-12-05 11:31:32 +0530

    add diamond pattern with approach 2

--- JavaPatternPractice/diamondPattern.java
+++ JavaPatternPractice/diamondPattern.java
@@ -2,30 +2,55 @@ package JavaPatternPractice;
 
 public class diamondPattern {
     public static void diamondPat(int n){
-        int count =0;
+        // this approach 1-: third count variable
+        // int count =0;
         // outer loops
-       for(int i=1;i<=n;i++){
-           for(int spaces=1;spaces<=n-i;spaces++){
-               System.out.print(" ");
-           }
-           for(int j=1;j<=count+i;j++){
-               System.out.print("*");
-           }
-           count++;
-           System.out.println();
-       }
+    //    for(int i=1;i<=n;i++){
+    //        for(int spaces=1;spaces<=n-i;spaces++){
+    //            System.out.print(" ");
+    //        }
+    //        for(int j=1;j<=count+i;j++){
+    //            System.out.print("*");
+    //        }
+    //        count++;
+    //        System.out.println();
+    //    }
     //   2nd half
-    int count2=n;
-       for(int i=n;i>=1;i--){
-           for(int spaces=1;spaces<=n-i;spaces++){
-               System.out.print(" ");
-           }
-           for(int j=1;j<=count2+(i-1);j++){
-               System.out.print("*");
-           }
-          count2= count2-1;
-           System.out.println();
-       }
+    // int count2=n;
+    //    for(int i=n;i>=1;i--){
+    //        for(int spaces=1;spaces<=n-i;spaces++){
+    //            System.out.print(" ");
+    //        }
+    //        for(int j=1;j<=count2+(i-1);j++){
+    //            System.out.print("*");
+    //        }
+    //       count2= count2-1;
+    //        System.out.println();
+    //    }
+
+
+
+    // this approach 2
+    for(int i=1;i<=n;i++){
+        // spaces
+        for(int j=1;j<=(n-i);j++){
+            System.out.print(" ");
+        }
+        // for stars
+        for(int j=1;j<=2*i-1;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=n-i;j++){
+            System.out.print( " ");
+        }
+        for(int j=1;j<=2*i-1;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
     }
     public static void main(String[] args) {
         diamondPat(4);
