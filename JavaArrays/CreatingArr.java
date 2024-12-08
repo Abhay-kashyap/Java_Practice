dd32fc22b6e15a45975f5f64f0df6bcd18200f9e
Abhay-kashyap <abhaykashyap265@gmail.com>
2024-12-08 23:15:54 +0530

    add print subarray

---
+++ JavaArrays/CreatingArr.java
@@ -0,0 +1,31 @@
+package JavaArrays;
+
+import java.util.Scanner;
+
+public class CreatingArr {
+    public static void main(String[] args) {
+        // int marks[]=new int[50];
+        // int numbers[]={1,2,3};
+        // int moreNumbers[]={4,5,6};
+        // String fruits[]={"apple","mango"};
+      
+        // array insertion ,output ,updation
+        Scanner sc=new Scanner(System.in);
+        // int phy;
+        // phy=sc.nextInt();
+        
+        // int marks[]=new int[100];
+        // marks[0]=sc.nextInt();//phy
+        // marks[1]=sc.nextInt();//chem
+        // marks[2]=sc.nextInt();//math
+        // System.out.println("phy : " +marks[0]);
+        // System.out.println("chem : " +marks[1]);
+        // System.out.println("math : " +marks[2]);
+        //updation 
+        // marks[2]=marks[2]+1;
+        // System.out.println("math : " +marks[2]);
+        // int percentage=(marks[0]+marks[1]+marks[2])/3;
+        // System.out.println("percentage = " + percentage + "%");
+       
+    }
+}
